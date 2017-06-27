edef9bf4013c985434d5f9a68382ab61d2187584
AbhinavChennapareddy <Chennapareddy.Abhinav@gmail.com>
2017-06-27 03:37:35 -0500

    Added working SPI communication code between NXP board and Arduino Mega (Voltage)

--- Code/Arduino/SPI_EXAMPLE1/SPI_EXAMPLE1.ino
+++ Code/Arduino/SPI_EXAMPLE1/SPI_EXAMPLE1.ino
@@ -1,39 +1,83 @@
 
 #include <SPI.h>
 
-
-byte dataIN;
-byte HByte;
-byte LByte;
-int RPM;
-byte ff=0xFF;
-
+char tmp[2];
+uint8_t buf [4];
+volatile byte pos;
+volatile boolean process_it;
+  int voltage;
 
 void setup (void)
 {
-  Serial.begin (9600);   // debugging
-  Serial.println("Debugging Start");
+  Serial.begin (115200);   // debugging
   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
- 
+  
+  // turn on SPI in slave mode
   SPCR |= bit (SPE);
 
   // have to send on master in, *slave out*
   pinMode(MISO, OUTPUT);
-} 
+  
+  // get ready for an interrupt 
+  pos = 0;   // buffer empty
+  process_it = false;
+
+  // now turn on interrupts
+  SPCR |= bit(SPIE);
+
+}  // end of setup
+
+void printHex(int num, int precision) {
+     char tmp[16];
+     char format[128];
+
+    sprintf(format, "0x%%.%dX", precision);
+
+    sprintf(tmp, format, num);
+     Serial.println(tmp);
+}
+
+
+ //SPI interrupt routine
+ISR (SPI_STC_vect)
+{
+  if (pos < (sizeof (buf)/* - 1*/))
+    {
+        buf [pos++] = SPDR;
+    }
+  // example: newline means time to process buffer
+ 
+//  if (SPDR == 0x07)
+//  {
+//      tmp[0] = SPDR;
+//  }
+//  else if (SPDR == 0x1C)
+//  {
+//    tmp[1] = SPDR;
+//    if (tmp[0] == 0x07)
+//    {
+//      process_it = true;
+//    }
+//  }
+
+}  // end of interrupt routine SPI_STC_vect
+
+// main loop - wait for flag set in interrupt routine
 void loop (void)
 {
 
+//
+//  if (1)
+//    {
+    
+    voltage = ((short)buf[1]*256)+buf[3];
+    if(voltage > 32767)
+       voltage = voltage - 65536;
+       Serial.print("Voltage: ");  
+       Serial.println(voltage);
 
-dataIN = SPDR;
-HByte = 0x000B;//dataIN>>4; 
-LByte = 0x000A;//dataIN<<4;
-//LByte = LByte>>4;
-RPM = (int)HByte*256+(int)LByte;
-if (RPM>32767)
-  RPM=RPM-65536;
-  
-//Serial.println(LByte);
-//Serial.println(HByte);
-Serial.println(RPM);
-//Serial.println((HByte<<4)+LByte);
-}
+    pos = 0;
+//    process_it = false;
+//    }
+    
+}  // end of loop
