e9a39db80f86690ffa9560b5b384b6205e3cc23c
UTDerrick <sxh128130@utdallas.edu>
2017-07-03 16:59:21 -0500

    Added SHIFTER code and SHIFT with SPI enabled code. Cleaned up LED with SPI comments and orginized them for better reading Included a comment note about SPI error issue to explore a fix for as per Dr. Bahita

--- Code/Arduino/RGB_PRM_V2_SPI_Enabled/RGB_PRM_V2_SPI_Enabled.ino
+++ Code/Arduino/RGB_PRM_V2_SPI_Enabled/RGB_PRM_V2_SPI_Enabled.ino
@@ -5,47 +5,68 @@
 For RGB pins LOW = ON, HIGH = OFF
 each segment represents 210 RPM incriments
 
+LED PIN - NAME - MCU PIN                1   -   2   -  3
+      1 -  C1  - 46            RPM - ..210 - ..420 - ..630
+     25 -  C2  - 48            RPM - ..840 - .1050 - .1260
+      3 -  C3  - 49            RPM - .1470 - .1680 - .1890
+      4 -  C4  - 47            RPM - .2100 - .2310 - .2520
+     23 -  C5  - 45            RPM - .2730 - .2940 - .3150
+      5 -  C6  - 43            RPM - .3360 - .3570 - .3780
+     22 -  C7  - 41            RPM - .3990 - .4200 - .4410
+     21 -  C8  - 39            RPM - .4620 - .4830 - .5040
+     20 -  C9  - 37            RPM - .5250 - .5460 - .5670
+      8 -  C10 - 33            RPM - .5880 - .6090 - .6300
+     19 -  C11 - 35            RPM - .6510 - .6720 - .6930
+     18 -  C12 - 31            RPM - .7140 - .7350 - .7560
+     10 -  C13 - 28            RPM - .7770 - .7980 - .8190
+     11 -  C14 - 26            RPM - .8400 - .8610 - .8820
+     15 -  C15 - 24            RPM - .9030 - .9240 - .9450
+     12 -  C16 - 22            RPM - .9660 - .9870 - 10080
+
 */
 
 
-                          //GLOBAL vars and pins
-                                                 //7SEG pin setup
-#define sA 40  // 7                                       _ 5
-#define sB 42  // 6                                     0|__| 6
-#define sC 32  // 4                                     4| 1|2
-#define sD 34  // 2                                       --3   .7
-#define sE 36  // 1
-#define sF 38  // 9
-#define sG 44  // 10
-#define sDP 30  // 5
-                          //Anodes  
+ //*********************** START of Global vars and pins and functions **********************************//
+                          
+              //7SEG pin setup  LED PIN - NAME - MCU PIN
+#define sA 40                     //  7 -  A   - 40                                  __ 5
+#define sB 42                     //  6 -  B   - 42                                0|__| 6
+#define sC 32                     //  4 -  C   - 32                                4| 1|2
+#define sD 34                     //  2 -  D   - 34                                  --3   .7
+#define sE 36                     //  1 -  E   - 36
+#define sF 38                     //  9 -  F   - 38
+#define sG 44                     // 10 -  G   - 44
+#define sDP 30                    //  5 -  DP  - 30
+                                  //3,8 -  VCC - 5V out
+                                  
+              //Anodes          LED PIN - NAME - MCU PIN
+#define aG1 A0                    // 24 -  G1  - A0 
+#define aG2 A1                    //  6 -  G2  - A1
+#define aG3 A2                    // 14 -  G3  - A2
+#define aB1 A3                    //  7 -  B1  - A3
+#define aB2 A5                    // 16 -  B2  - A5
+#define aB3 A4                    //  2 -  B3  - A4
+#define aR1 A6                    //  9 -  R1  - A6
+#define aR2 A7                    // 17 -  R2  - A7
+#define aR3 A8                    // 13 -  R3  - A8
 
-#define aG1 A0
-#define aG2 A1
-#define aG3 A2
-#define aB1 A3
-#define aB2 A5
-#define aB3 A4
-#define aR1 A6
-#define aR2 A7
-#define aR3 A8
-                        //Cathode Pin setup                                         1   -   2   -  3
-int C1 = 46;                                          //C1 - 46            RPM - ..210 - ..420 - ..630
-int C2 = 48;                                          //C2 - 48            RPM - ..840 - .1050 - .1260
-int C3 = 49;                                          //C3 - 49            RPM - .1470 - .1680 - .1890
-int C4 = 47;                                          //C4 - 47            RPM - .2100 - .2310 - .2520
-int C5 = 45;                                          //C5 - 45            RPM - .2730 - .2940 - .3150
-int C6 = 43;                                          //C6 - 43            RPM - .3360 - .3570 - .3780
-int C7 = 41;                                          //C7 - 41            RPM - .3990 - .4200 - .4410
-int C8 = 39;                                          //C8 - 39            RPM - .4620 - .4830 - .5040
-int C9 = 37;                                          //C9 - 23            RPM - .5250 - .5460 - .5670
-int C10 = 33;                                         //C10- 25            RPM - .5880 - .6090 - .6300
-int C11 = 35;                                         //C11- 27            RPM - .6510 - .6720 - .6930
-int C12 = 31;                                         //C12- 29            RPM - .7140 - .7350 - .7560
-int C13 = 28;                                         //C13- 31            RPM - .7770 - .7980 - .8190
-int C14 = 26;                                         //C14- 33            RPM - .8400 - .8610 - .8820
-int C15 = 24;                                         //C15- 35            RPM - .9030 - .9240 - .9450
-int C16 = 22;                                         //C16- 37            RPM - .9660 - .9870 - 10080
+              //Cathode         LED PIN - NAME - MCU PIN       
+#define C1 46                     //  1 -  C1  - 46            
+#define C2 48                     // 25 -  C2  - 48            
+#define C3 49                     //  3 -  C3  - 49            
+#define C4 47                     //  4 -  C4  - 47            
+#define C5 45                     // 23 -  C5  - 45            
+#define C6 43                     //  5 -  C6  - 43            
+#define C7 41                     // 22 -  C7  - 41            
+#define C8 39                     // 21 -  C8  - 39            
+#define C9 37                     // 20 -  C9  - 37            
+#define C10 33                    //  8 -  C10 - 33            
+#define C11 35                    // 19 -  C11 - 35            
+#define C12 31                    // 18 -  C12 - 31            
+#define C13 28                    // 10 -  C13 - 28            
+#define C14 26                    // 11 -  C14 - 26            
+#define C15 24                    // 15 -  C15 - 24            
+#define C16 22                    // 12 -  C16 - 22            
 
                           //Arrays for clean Setup intilization code
 int cathode[] = {C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13,C14,C15,C16};
@@ -56,130 +77,45 @@ int green[] = {aG1,aG2,aG3};
 int cLen = 16;
 int sLen = 8;
 int aLen = 3;
-                                        //RPM Simulation variable PIN
-int RPM_PIN = 7;
+                                                                    
+//int RPM_PIN = 7;                                                  //RPM Simulation variable PINOUT not used when running SPI
 int RPM = 0;
-int RPM_IDLE = 0;                     //actual car values will be 600
-int RPM_REDLINE=4700;                  //actual car values will be 10000    for testing with volatge input we will use 1400
+int RPM_IDLE = 0;                                                   //actual car values will be 600
+int RPM_REDLINE=4700;                                               //actual car values will be 10000    for testing with volatge input we will use 1400
 
-/////////////////////////////////////////SPI Define start
-volatile byte pos;                      //used for SPI buffer position, its set to global so it can be reset after populated to RPM array
+/////////////////////////////////////////////////////               START of SPI Define start
+
+volatile byte pos;                                                  //used for SPI buffer position, its set to global so it can be reset after populated to RPM array
 uint8_t buf[4];
-
-                                       //**********************SPI interrupt routine************************************
+                                                                    //Start of Interrupt routine (ISR)
 ISR (SPI_STC_vect)
 {
-  if (pos < (sizeof (buf)))            //Gets SPI data value (RPM) and places in array (buf)
+  if (pos < (sizeof (buf)))                                         //Gets SPI data value (RPM) and places in array (buf)
     {
-        buf [pos++] = SPDR;
+        buf [pos++] = SPDR;                                         //Populates buffer array with bytes at pos less then buff size
     }
-}                                      // end of interrupt routine SPI_STC_vect
+}                                                                   // end of interrupt routine SPI_STC_vect
 
 int raw2int()
 {
   int data_packet = ((short)buf[1]<<8)+buf[3];
   if(data_packet > 32767)
     data_packet = data_packet - 65536;
-    pos = 0;                                                                    //Global var to reset buffer count in interupt
-    Serial.print("Converted Data from raw SPI: ");                              //Debugging serial output for raw data conversion
+    pos = 0;                                                        //Global pos variable set to 0 to reset buffer count for next interrupt buf population
+    Serial.print("Converted Data from raw SPI: ");                  //Debugging serial output for raw data conversion
     Serial.println(data_packet);
-    return(data_packet);
+    return(data_packet);                                            //Returns signed integer value after conversion via PE3's datasheet for CAN messages
 }
-//////////////////////////////////////SPI define end
+/*
+ * Fix for error posibility: create pesudo ACK fedback through a GPIO pin OR shorten up the BYTES sent and be strict about BYTE matching
+ * maybe use a loop check to verify bytes being sent are never whitespace HEX value (reminder set serial to output all HEX values ONLY including those for 
+ * white space and NEWLINE commands) 
+ */
+
+////////////////////////////////////////////////////////            END of SPI define
 
 
-
-                                       //************************SETUP LOOP************************************
-void setup() 
-{   
-       // 7 Segment pinout control
-
-  for(int sPin = 0; sPin < sLen; sPin++)
-      {
-        pinMode(seg[sPin],OUTPUT);
-      }
-  
-           //RGB Anode control 
-  
-  for (int aPin = 0; aPin < aLen; aPin++) 
-      {
-        pinMode(red[aPin], OUTPUT);
-        pinMode(green[aPin], OUTPUT);
-        pinMode(blue[aPin], OUTPUT);
-      }
-
-              //Cathode Control
-                                      
-    for (int cPin = 0; cPin < cLen; cPin++) 
-      {
-        pinMode(cathode[cPin], OUTPUT);
-      }
-  
-  //DEBUGGING Stuff
-  //pinMode(RPM_PIN,INPUT);     //Potentiameter input
-  Serial.begin(115200);       //  setup serial 
-               
-  initialize();               //not needed anymore but looks cool
-                    
-                    
-                    
-                    //SPI Setup
-  SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE0));
-  SPCR |= bit (SPE);     // turn on SPI in slave mode
-  pinMode(MISO, OUTPUT); // have to send on master in, *slave out*
-  pos = 0;   // buffer empty to get ready for interrupts
-  SPCR |= bit(SPIE); // now turn on interrupts
-  
-}
-
-
-
-
-
-                                       //**********************main loop ***********************************************
-
-void loop() 
-    {
-
-          //RPM = analogRead(RPM_PIN);      //Use only for POT Demo mode SPI disabled
-          //RPM = RPM*10;
-
-          RPM = raw2int();           //Use when SPI enabled
-          int ledLevel = map(RPM,RPM_IDLE,RPM_REDLINE, 0, cLen);
-          
-          resetLED();
- 
-          if(RPM <=2500)
-            greenLED(LOW);
-          if(RPM >= 2501 && RPM <=4000)
-            blueLED(LOW);
-          if(RPM >= 4001)
-            redLED(LOW);
-                                                                               
-
-  // loop over the LED array:
-  for (int thisLed = 0; thisLed < cLen; thisLed++) 
-    {
-    // if the array element's index is less than ledLevel,
-    // turn the pin for this element on:
-      if (thisLed < ledLevel) 
-        {
-          digitalWrite(cathode[thisLed],HIGH);
-          printSeg(ledLevel);
-        }
-        // turn off all pins higher than the ledLevel:
-      else 
-        {
-          digitalWrite(cathode[thisLed], LOW);
-        }
-  }
-}
-////////////////////////////////////////////////////////////////////////////////////
-
-
-//**************************LED FUNCTIONS****************************************//
-
-
+///////////////////////////////////////////////////////             START of LED functions
 void redLED(bool state)
 {
     for(int a = 0; a < aLen; a++)
@@ -220,9 +156,6 @@ void flashLED()
  
 }
 
-
-
-//////////////////////////////////////////////////////////////INITIALIZE
 void initialize()
 {
     for(int cath = 0; cath < cLen; cath++)
@@ -285,4 +218,97 @@ if(num > 12)      //sets any number given to function outside of its scope to tu
   digitalWrite(seg[led],segment[num][led]);
 }
 }
+///////////////////////////////////////////////////////             END of LED functions
+
+//*********************** END of Global vars and pins and functions **********************************//
+
+
+
+//************************ Start of Setup ************************************//
+void setup() 
+{   
+       // 7 Segment pinout control
+
+  for(int sPin = 0; sPin < sLen; sPin++)
+      {
+        pinMode(seg[sPin],OUTPUT);
+      }
+  
+           //RGB Anode control 
+  
+  for (int aPin = 0; aPin < aLen; aPin++) 
+      {
+        pinMode(red[aPin], OUTPUT);
+        pinMode(green[aPin], OUTPUT);
+        pinMode(blue[aPin], OUTPUT);
+      }
+
+              //Cathode Control
+                                      
+    for (int cPin = 0; cPin < cLen; cPin++) 
+      {
+        pinMode(cathode[cPin], OUTPUT);
+      }
+  
+  //DEBUGGING Stuff
+  //pinMode(RPM_PIN,INPUT);     //Potentiameter input
+  Serial.begin(115200);       //  setup serial 
+               
+  initialize();               //not needed anymore but looks cool
+                    
+                    
+                    
+/////////////////////////////////////////////////////////////////               START of SPI Setup
+SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE0));
+SPCR |= bit (SPE);                                                            // turn on SPI in slave mode
+pinMode(MISO, OUTPUT);                                                        // have to send on master in, *slave out*
+pos = 0;                                                                      // buffer empty to get ready for interrupts
+SPCR |= bit(SPIE);                                                            // now turn on interrupts
+/////////////////////////////////////////////////////////////////               END of SPI Setup  
+  
+}
+
+//************************ END of Setup ************************************//
+
+
+//************************ Main loop **************************************//
+
+void loop() 
+    {
+
+          //RPM = analogRead(RPM_PIN);      //Use only for POT Demo mode SPI disabled
+          //RPM = RPM*10;
+
+          RPM = raw2int();           //Use when SPI enabled
+          int ledLevel = map(RPM,RPM_IDLE,RPM_REDLINE, 0, cLen);
+          
+          resetLED();
+ 
+          if(RPM <=2500)
+            greenLED(LOW);
+          if(RPM >= 2501 && RPM <=4000)
+            blueLED(LOW);
+          if(RPM >= 4001)
+            redLED(LOW);
+                                                                               
+
+  // loop over the LED array:
+  for (int thisLed = 0; thisLed < cLen; thisLed++) 
+    {
+    // if the array element's index is less than ledLevel,
+    // turn the pin for this element on:
+      if (thisLed < ledLevel) 
+        {
+          digitalWrite(cathode[thisLed],HIGH);
+          printSeg(ledLevel);
+        }
+        // turn off all pins higher than the ledLevel:
+      else 
+        {
+          digitalWrite(cathode[thisLed], LOW);
+        }
+  }
+}
+//**************************END of Main Loop*************************************//
+
 
---
+++ Code/Arduino/shifter_with_SPI_Enabled/shifter_with_SPI_Enabled.ino
@@ -0,0 +1,115 @@
+/////////////////////////////////////////////////////               START of SPI Define start
+#include <SPI.h>
+volatile byte pos;                                                  //used for SPI buffer position, its set to global so it can be reset after populated to RPM array
+uint8_t buf[4];
+                                                                    //Start of Interrupt routine (ISR)
+ISR (SPI_STC_vect)
+{
+  if (pos < (sizeof (buf)))                                         //Gets SPI data value (RPM) and places in array (buf)
+    {
+        buf [pos++] = SPDR;                                         //Populates buffer array with bytes at pos less then buff size
+    }
+}                                                                   // end of interrupt routine SPI_STC_vect
+
+int raw2int()
+{
+  int data_packet = ((short)buf[1]<<8)+buf[3];
+  if(data_packet > 32767)
+    data_packet = data_packet - 65536;
+    pos = 0;                                                        //Global pos variable set to 0 to reset buffer count for next interrupt buf population
+    Serial.print("Converted Data from raw SPI: ");                  //Debugging serial output for raw data conversion
+    Serial.println(data_packet);
+    return(data_packet);                                            //Returns signed integer value after conversion via PE3's datasheet for CAN messages
+}
+////////////////////////////////////////////////////////             END of SPI define
+  
+  const int forwards = 2;
+  const int backwards = 4;
+  const int up = 0;
+  const int down = 1;
+  int RPM_Pin = 0;
+  bool shiftup = 0;
+  bool shiftdown = 0;
+  const int ClutchOutsert = 8;   // HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA best var name ever!!
+  const int ClutchInsert = 7;
+  int RPM;
+
+void setup() {
+
+pinMode(forwards, OUTPUT);//Set relay as an output
+pinMode(backwards, OUTPUT);//Set relay as an output
+pinMode(ClutchInsert, OUTPUT);
+pinMode(ClutchOutsert, OUTPUT);
+pinMode(up, INPUT);
+pinMode(down, INPUT);
+pinMode(RPM_Pin, INPUT);
+Serial.begin(19200);
+
+
+/////////////////////////////////////////////////////////////////               START of SPI Setup
+SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE0));
+SPCR |= bit (SPE);                                                            // turn on SPI in slave mode
+pinMode(MISO, OUTPUT);                                                        // have to send on master in, *slave out*
+pos = 0;                                                                      // buffer empty to get ready for interrupts
+SPCR |= bit(SPIE);                                                            // now turn on interrupts
+/////////////////////////////////////////////////////////////////               END of SPI Setup  
+}
+
+void loop() 
+{
+  //RPM = analogRead(RPM_Pin);                                                  // RPM value set to be analog value of RPM given by Simulation POT
+  RPM = raw2int();                                                              // RPM value set to be numerical value of ECU given RPM
+
+
+  int num = analogRead(up);
+  int num2 = analogRead(down);
+  Serial.println(num2);
+  if(num > 900)
+  {
+    digitalWrite(ClutchOutsert, LOW);
+    digitalWrite(ClutchInsert, HIGH);
+    delay(500);
+    digitalWrite(forwards,HIGH);
+    digitalWrite(backwards,LOW);
+    delay(110);
+    digitalWrite(forwards,LOW);
+    digitalWrite(backwards,HIGH);
+    delay(110);
+    digitalWrite(forwards,HIGH);
+    digitalWrite(backwards,HIGH);
+    digitalWrite(ClutchOutsert, HIGH);
+    digitalWrite(ClutchInsert, LOW);
+    delay(500);
+    
+  }
+  else
+   digitalWrite(forwards,HIGH);
+   digitalWrite(backwards,HIGH);
+   digitalWrite(ClutchOutsert, HIGH);
+   digitalWrite(ClutchInsert, HIGH);
+  
+  if(num2 > 900)
+  {
+    digitalWrite(ClutchOutsert, LOW);
+    digitalWrite(ClutchInsert, HIGH);
+    delay(500);
+    digitalWrite(forwards,LOW);
+    digitalWrite(backwards,HIGH);
+    delay(110);
+    digitalWrite(forwards,HIGH);
+    digitalWrite(backwards,LOW);
+    delay(110);
+    digitalWrite(forwards,HIGH);
+    digitalWrite(backwards,HIGH);
+    digitalWrite(ClutchOutsert, HIGH);
+    digitalWrite(ClutchInsert, LOW);
+    delay(500);
+  }
+  else 
+   digitalWrite(backwards,HIGH);
+   digitalWrite(forwards,HIGH);
+   digitalWrite(ClutchInsert,HIGH);
+   digitalWrite(ClutchOutsert,HIGH);
+}
+ 
+
