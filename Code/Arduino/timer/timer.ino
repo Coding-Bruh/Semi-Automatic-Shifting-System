397e2f2e02d5b3411047e650edfb12e7eb979e1a
AbhinavChennapareddy <Chennapareddy.Abhinav@gmail.com>
2017-07-28 22:33:24 -0500

    added another hall effect sensor to the shift speed module

--- Code/Arduino/timer/timer.ino
+++ Code/Arduino/timer/timer.ino
@@ -1,12 +1,14 @@
-const int hallPin = 7;     // the number of the hall effect sensor pin
-int hallState1 = 0;          // variable for reading the hall sensor status
+const int hallPin1 = 7;     // the number of the hall effect sensor pin
+const int hallPin2 = 8;     // the number of the hall effect sensor pin
+int hallState1 = 0;         // variable for reading the hall sensor status
+int hallState2 = 0;         // variable for reading the hall sensor status
 long startTime ;                    // start time for stop watch
 long elapsedTime ;                  // elapsed time for stop watch
-bool process_done = false;
 void setup() {
 Serial.begin(9600);
 
-pinMode(hallPin, INPUT); // initialize the hall effect sensor pin as an input:
+pinMode(hallPin1, INPUT); // initialize the hall effect sensor pin as an input
+pinMode(hallPin2, INPUT); // initialize the hall effect sensor pin as an input
 
 
 }
@@ -15,14 +17,15 @@ void loop() {
 
   
   // read the state of the hall effect sensor:
-  hallState1 = digitalRead(hallPin);
+  hallState1 = digitalRead(hallPin1);
+  hallState2 = digitalRead(hallPin2);
   
   if (hallState1 == LOW)
   {     
     // Store the start time
     startTime = micros();
   } 
-  else if (hallState1 == HIGH)
+  if (hallState2 == LOW)
   {
     // Store the elapsed time
     elapsedTime = micros() - startTime;
